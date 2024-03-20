9916e5a779733226e5f45a1c99cf21885a4b39dc
joelo <joelonrichardson2006@gmail.com>
2024-03-20 23:07:54 +0100

    new

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "Tp_ModBus_Richardson"
+
---
+++ src/main/java/fr/btsciel/Main.java
@@ -0,0 +1,20 @@
+package fr.btsciel;
+
+import classeModbusRTU.ClasseModbus;
+import jssc.SerialPortException;
+
+public class Main {
+    static ClasseModbus modbus;
+    public static void main(String[] args){
+        System.out.println("le n° de l'esclave svp");
+        modbus = new ClasseModbus(In.readByte());
+        System.out.println("le com svp");
+        try {
+            modbus.connecEsclave(In.readString(),9600,8, 0, 1);
+            modbus.lectureCoils(8192,2);
+        } catch (SerialPortException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
